ca89e53808d2309f2506752af6e9315ff7ff3698
Deepjyoti Borah <dborah@equalexperts.com>
2019-10-25 14:27:04 +0530

    Changes

--- azureDevOps/main.tf
+++ azureDevOps/main.tf
@@ -154,7 +154,7 @@ resource "azurerm_public_ip" "demoLBPublicIP" {
   name                = "demoLBPublicIP"
   location            = "${azurerm_resource_group.demoResourceGroup_Deep.location}"
   resource_group_name = "${azurerm_resource_group.demoResourceGroup_Deep.name}"
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "demoLoadBalancer" {
